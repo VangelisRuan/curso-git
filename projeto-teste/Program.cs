0c9a7cbefbc83e18e31e5439f2cba6afae2358db
Vangelis Ruan <vangelis.projects@hotmail.com>
2021-04-05 08:55:59 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
